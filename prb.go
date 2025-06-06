ec028c3690ad17eb78eb7c8734db59bedeedce1b
Daniel Havrda <Havrda@damidev.com>
2025-06-06 02:24:49 +0200

    feat: add implementation for Insert() function

--- prb.go
+++ prb.go
@@ -17,7 +17,7 @@ type PriorityRingBuffer[T any] struct {
 	head, tail     int
 	size           int
 	bubbleWindow   int
-	orderCounter   int
+	orderCounter   int64
 	overwriteGuard bool
 	mu             *sync.Mutex
 }
@@ -29,7 +29,7 @@ type Config struct {
 	ThreadSafe     bool
 }
 
-func NewPRB[T any](config Config) *PriorityRingBuffer[T] {
+func New[T any](config Config) *PriorityRingBuffer[T] {
 	var mu *sync.Mutex
 	if config.ThreadSafe {
 		mu = &sync.Mutex{}
@@ -55,7 +55,42 @@ func (b *PriorityRingBuffer[T]) unlock() {
 	}
 }
 func (b *PriorityRingBuffer[T]) Insert(value T, priority int) error {
+	b.lock()
+	defer b.unlock()
 
+	element := Element[T]{Value: value, Priority: priority, InsertionOrder: b.orderCounter}
+	b.orderCounter++
+
+	overwriting := b.size == b.capacity
+	insertIndex := b.tail
+
+	if overwriting && b.overwriteGuard {
+		if priority <= b.elements[b.head].Priority {
+			return errors.New("buffer is full, refused to overwrite higher priority element")
+		}
+	}
+
+	b.elements[insertIndex] = element
+
+	for i := 1; i <= b.bubbleWindow && b.size > 0; i++ {
+		previousIndex := (insertIndex - 1 + b.capacity) % b.capacity
+		previousElement := b.elements[previousIndex]
+		currentElement := b.elements[insertIndex]
+		if currentElement.Priority > previousElement.Priority || currentElement.InsertionOrder < previousElement.InsertionOrder {
+			b.elements[insertIndex], b.elements[previousIndex] = b.elements[previousIndex], b.elements[insertIndex]
+			insertIndex = previousIndex
+		} else {
+			break
+		}
+	}
+
+	b.tail = (b.tail + 1) % b.capacity
+	if overwriting {
+		b.head = b.tail
+	} else {
+		b.size++
+	}
+	return nil
 }
 
 func (b *PriorityRingBuffer[T]) Dequeue() (Element[T], error) {
@@ -66,7 +101,7 @@ func (b *PriorityRingBuffer[T]) PeekHead() (Element[T], error) {
 	b.lock()
 	defer b.unlock()
 	if b.size == 0 {
-		return Element[T]{}, errors.New("Buffer is empty")
+		return Element[T]{}, errors.New("buffer is empty")
 	}
 	return b.elements[b.head], nil
 }
@@ -77,6 +112,7 @@ func (b *PriorityRingBuffer[T]) PeekMaxPriority() (Element[T], error) {
 func (b *PriorityRingBuffer[T]) Search(value *T, priority *int) []int {
 
 }
+
 func (b *PriorityRingBuffer[T]) Size() int {
 	b.lock()
 	defer b.unlock()
